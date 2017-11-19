0e69e4acf4ba076ae8e15e8d8203e180125e75b4
Kevin Le <17786629@student.westernsydney.edu.au>
2017-11-19 05:22:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,10 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     false
+  elsif board[index] == ""
+    false
+  elsif  board[index] == nil
+    false
   else   board[index] == "X" || "O"
     true
 end
